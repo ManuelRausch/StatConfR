38e0829758006b2b1bfd60d9582b306487bf302b
Manuel Rausch <114725315+ManuelRausch@users.noreply.github.com>
2023-08-02 11:41:22 +0200

    Example data set

---
+++ R/MaskOri-data.R
@@ -0,0 +1,23 @@
+#' Data of 36 participants in a masked orientation discrimination experiment
+#'
+#' In each trial, participants were shown a sinusoidal grating oriented either horizontally or vertically, followed by a mask after varying stimulus-onset-asynchronies.
+#' Participants were instructed to report the orientation and their degree of confidence as accurately as possible
+#' participant: Participant-ID
+#' stimulus: The orientation of the grating. 90 means vertical and 0 means horizontal.
+#' correct: 1 means a correct orientation response, 0 incorrect orientation response
+#' rating:  4-point confidence scale. The four confidence categories were labelled as "not at all", "a little", "nearly sure" and "sure".
+#'
+#' @docType data
+#'
+#' @usage data(MaskOri)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references TO DO
+
+#' @examples
+#' data(MaskOri)
+#' summary(MaskOri)
+"MaskOri"
